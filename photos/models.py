b21d562c660359a70f7aa379a65af9c4b4b6bb81
foleary56 <finnoleary56@icloud.com>
2024-10-22 13:17:06 -0400

    models.py with Job and Photo models

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.utils import timezone
+import os
 
-# Create your models here.
+class Job(models.Model):
+    name = models.CharField(max_length=255)
+    # Add other fields as necessary
+
+    def __str__(self):
+        return self.name
+
+def get_upload_path(instance, filename):
+    job_name = instance.job.name.replace(" ", "_")
+    photo_type = instance.photo_type
+    date_uploaded = timezone.now().strftime("%Y-%m-%d")
+    return os.path.join(f'{job_name}/{photo_type}/{date_uploaded}', filename)
+
+class Photo(models.Model):
+    PHOTO_TYPE_CHOICES = [
+        ('before', 'Before'),
+        ('during', 'During'),
+        ('after', 'After'),
+        ('issue', 'Issue'),
+    ]
+    
+    job = models.ForeignKey(Job, on_delete=models.CASCADE, related_name='photos')
+    photo_type = models.CharField(max_length=6, choices=PHOTO_TYPE_CHOICES)
+    image = models.ImageField(upload_to=get_upload_path)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.photo_type.capitalize()} photo for {self.job.name}"
