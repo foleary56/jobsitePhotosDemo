cfbc9468e125d7a46f75559b1f368dd6f6e1bd02
foleary56 <finnoleary56@icloud.com>
2024-10-22 13:26:28 -0400

    forms.py and views.py setup

--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import PhotoUploadForm
 
-# Create your views here.
+def upload_photo(request):
+    if request.method == 'POST':
+        form = PhotoUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('upload_success')
+    else:
+        form = PhotoUploadForm()
+    return render(request, 'upload_photo.html', {'form': form})
+
+def upload_success(request):
+    return render(request, 'upload_success.html')
